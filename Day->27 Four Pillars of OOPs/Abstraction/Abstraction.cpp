1b07163e59c97b3b326b1e1a5e2664ae903daa53
SudhanshuYadav <shudhanshuyadav48@gmail.com>
2025-01-27 22:34:24 +0530

    Add files via upload

---
+++ Day->27 Four Pillars of OOPs/Abstraction/Abstraction.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class SumsangF34_5G{
+	private:
+		void Company_Employees_Count(){
+			cout<<"More Than 2,60,000"<<endl;
+		}
+		void country_name(){
+			cout<<"South Korea"<<endl;
+		}
+	public:
+		void company_name(){
+			cout<<"Samsung"<<endl;
+		}
+		void model(){
+			cout<<"Samsung F34 5G"<<endl;
+		}
+		void color(){
+			cout<<"Gray/Black/Blue"<<endl;
+		}
+		void cost(){
+			cout<<"17000"<<endl;
+		}
+		void changer_type(){
+			cout<<"C to C type"<<endl;
+		}
+		
+};
+int main(){
+	SumsangF34_5G s;
+	s.company_name();
+	s.model();
+	s.color();
+	s.cost();
+	s.changer_type();
+}
